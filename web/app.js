e920bbabe18d25d9a35cd74174eea684e0919846
Lowkalol <92874536+krittapoj@users.noreply.github.com>
2024-02-12 20:19:56 +0700

    11/02/2024

--- web/app.js
+++ web/app.js
@@ -1,6 +1,9 @@
 {
-    let loop_number = prompt("ใส่เลข");
+   /* let loop_number = prompt("ใส่เลข");
     console.log(loop_number );
     console.log(Math.floor(Math.random() * 100));
-    document.getElementById("result").innerHTML = Math.floor(Math.random() * 100);
+    document.getElementById("result").innerHTML = Math.floor(Math.random() * 100);*/
 }
+
+
+    
