6266505fefa17a24117487af3ec86702ff66c24b
Lowkalol <92874536+krittapoj@users.noreply.github.com>
2024-02-14 20:30:47 +0700

    function

--- web/function.js
+++ web/function.js
@@ -1,4 +1,13 @@
+function toCelsius()
+{
+    let fahrenheit = prompt("Fahrenheit")
+    let value = (fahrenheit * -3) * 5 / 9
+    document.getElementById("fuc").innerHTML = value.toFixed(2) + "c";
+}
+toCelsius()
+
 function sayhello()
 {
-    
+    return "Hello"
 }
+alert(sayhello())
