05f240c149ff2f488cae373b779734851838bf7f
Lowkalol <92874536+krittapoj@users.noreply.github.com>
2024-02-15 20:06:30 +0700

    function 2

--- web/function.js
+++ web/function.js
@@ -1,13 +1,14 @@
-function toCelsius()
+function toCelsius(fahrenheit)
 {
     let fahrenheit = prompt("Fahrenheit")
     let value = (fahrenheit * -3) * 5 / 9
     document.getElementById("fuc").innerHTML = value.toFixed(2) + "c";
 }
-toCelsius()
 
-function sayhello()
+/*Parameter = ตัวแปรที่เกิดมาพร้อมกับ function มีหน้าที่เอาไว้ใช้งานภายใน function นั้นๆ
+Atgument = ข้อมูลที่ส่งให้ function*/
+function sayhello(userName, userlastName)
 {
-    return "Hello"
+    return "Hello" + userName + userlastName
 }
-alert(sayhello())
+alert(sayhello("name","lastname"))
